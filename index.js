2cf9078bb7b7e1fdb5b826ceac57f30bd6ac878c
HP <20060339@stu.omu.edu.tr>
2023-02-21 00:05:41 +0300

    edit

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ function arkaplan() {
 }
 
 
+function arrow (nums) {
+  let arr = [];
+  nums.forEach(nums => {
+    if(nums%2 == 0){
+
+      return arr.push(`${nums*2}`)
+    }
+    else {
+      return arr.push(`${nums*3}`)
+    }
+    
+  });
+
+  return `[${arr}]`
+}
+
+
+document.write(arrow([1,2,3,4,5]));
+
+
