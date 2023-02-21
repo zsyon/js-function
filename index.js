341de3c52912dff2691c7346facbbc76f6b178f9
HP <20060339@stu.omu.edu.tr>
2023-02-21 19:05:49 +0300

    edit

--- index.js
+++ index.js
@@ -38,7 +38,14 @@ function sayıArttır(){
   console.log(sayi)
 }
 
-
+function sayıAzalt(){
+  console.log("aaaa")
+  sayi = document.getElementById("sayı").innerHTML
+  console.log(sayi)
+  sayi = Number(sayi) - 1
+  document.getElementById("sayı").innerHTML = sayi
+  console.log(sayi)
+}
 
 function arrow (nums) {
   let arr = [];
